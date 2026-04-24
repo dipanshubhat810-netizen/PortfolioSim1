a87da26f4f00e856d009bec97fb25b3d72bb280b
dipanshubhat810-netizen <dipanshubhat810@gmail.com>
2026-04-25 01:53:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,8 @@
-import os
-from dotenv import load_dotenv
+import streamlit as st
 
-# Load .env file for local development
-load_dotenv()
-
-# config.py — Use environment variables for deployment; defaults for local dev
-DB_HOST     = os.getenv("DB_HOST", "localhost")
-DB_USER     = os.getenv("DB_USER", "root")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "Atharva@2005")
-DB_NAME     = os.getenv("DB_NAME", "PortfolioSim1")
-
-RISK_FREE_RATE        = 0.03
-TOP_ASSETS_PER_SECTOR = 2
-DEFAULT_CORRELATION   = 0.10
+DB_HOST = st.secrets["mysql.railway.internal"]
+DB_PORT = int(st.secrets["3306"])
+DB_USER = st.secrets["root"]
+DB_PASSWORD = st.secrets["RjppinauDqPuZfmRjqPjCmkVoRUbhxIB"]
+DB_NAME = st.secrets["railway"]
 
