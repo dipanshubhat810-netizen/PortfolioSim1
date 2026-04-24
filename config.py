fe034bc62a260828464df87d201f98730f60e65d
dipanshubhat810-netizen <dipanshubhat810@gmail.com>
2026-04-25 02:24:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,11 @@
 import streamlit as st
 
-# -------------------------
-# Database config
-# -------------------------
 DB_HOST = st.secrets["DB_HOST"]
-DB_PORT = int(st.secrets.get("DB_PORT", 3306))
+DB_PORT = int(st.secrets["DB_PORT"])
 DB_USER = st.secrets["DB_USER"]
 DB_PASSWORD = st.secrets["DB_PASSWORD"]
 DB_NAME = st.secrets["DB_NAME"]
 
-# -------------------------
-# Portfolio config
-# -------------------------
-RISK_FREE_RATE        = 0.03
-TOP_ASSETS_PER_SECTOR = 2
-DEFAULT_CORRELATION   = 0.10
+RISK_FREE_RATE = 0.06
+TOP_ASSETS_PER_SECTOR = 5
+DEFAULT_CORRELATION = 0.25
